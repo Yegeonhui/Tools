d758d2abd35e7a451ada97ee89268b10ccee61ba
DESKTOP-4T7NFE7\Administrator <dprjsgml@gmail.com>
2022-08-18 11:06:52 +0900

    update select_river_valley

---
+++ 2022/Nakdong River water system management/Select_river_valley.py
@@ -0,0 +1,43 @@
+# 2022-08-17
+# 유역도에서 광려천, 주천강을 지나는 유역도만 불러와서 shp파일, geojson파일로 저장 
+# 특이하게 레코드를 먼저 선언해주고 poly를 넣는 형식
+# Code by YGH
+
+import shapefile
+from shapely.geometry import Polygon
+import geopandas 
+
+# 주천강 -> 죽동천597, 주천강610, 광려천 777
+idxarr = [596, 609, 776]
+
+# read 유역도
+shape = shapefile.Reader('수계_지적도/유역도 52N.shp')
+
+w = shapefile.Writer('new_유역도')
+
+# 레코드 추가, 텍스트 필드는 'C' 유형을 사용하여 생성
+for i in range(len(idxarr)):
+    w.field('name', 'C')
+
+for s in range(len(shape.shapeRecords())):
+    if s in idxarr:
+        feature = shape.shapeRecords()[s]
+        first = feature.shape.__geo_interface__
+        coordinates = first['coordinates']
+        w.poly(coordinates)
+
+for i in range(len(idxarr)):
+    w.record('polygon')
+
+w.close()
+
+shape = shapefile.Reader('new_유역도.shp')
+gdf = geopandas.GeoSeries([Polygon([]) for i in range(len(shape.shapeRecords()))])
+for s in range(len(shape.shapeRecords())):
+    feature = shape.shapeRecords()[s]
+    first = feature.shape.__geo_interface__
+    coordinates = first['coordinates'][0]
+    gdf[s] = Polygon(coordinates)
+
+gdf.crs = 32652
+gdf.to_file('new_유역도.geojson', driver='GeoJSON')
---
+++ 2022/Nakdong River water system management/readme.md
@@ -0,0 +1,19 @@
+# Select_river_valley
+> 유역도에서 광려천, 주천강을 지나는 유역도만 다시 저장
+
+## How to use?
+1. 죽동천, 주천강, 광려천의 인덱스를 구해서
+2. shapefile에서 해당부부만 가져와서 저장
+3. geojson으로 저장
+
+## install Library
+```
+pip install pyshp
+pip install shapely
+conda install geopandas
+```
+
+## Example_Image
+![Origin](Example_Image/%EC%9C%A0%EC%97%AD%EB%8F%84.PNG)
+![New](Example_Image/new_%EC%9C%A0%EC%97%AD%EB%8F%84.PNG.PNG)
+![GeoJSON](Example_Image/geojson.PNG.PNG)
