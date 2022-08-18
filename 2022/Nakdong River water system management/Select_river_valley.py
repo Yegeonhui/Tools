b2c38860ec45cbf8c66aa4acd71aa21706155af0
DESKTOP-4T7NFE7\Administrator <dprjsgml@gmail.com>
2022-08-18 16:48:54 +0900

    update code

--- 2022/Nakdong River water system management/Select_river_valley.py
+++ 2022/Nakdong River water system management/Select_river_valley.py
@@ -1,41 +1,16 @@
 # 2022-08-17
 # 유역도에서 광려천, 주천강을 지나는 유역도만 불러와서 shp파일, geojson파일로 저장 
-# 특이하게 레코드를 먼저 선언해주고 poly를 넣는 형식
 # Code by YGH
 
-import shapefile
-from shapely.geometry import Polygon
 import geopandas 
 
-def saveshapefile(shape, saveroute):
-    w = shapefile.Writer(saveroute)
-
-    # 레코드 추가, 텍스트 필드는 'C' 유형을 사용하여 생성
-    for i in range(len(idxarr)):
-        w.field('name', 'C')
-
-    for s in range(len(shape.shapeRecords())):
-        if s in idxarr:
-            feature = shape.shapeRecords()[s]
-            first = feature.shape.__geo_interface__
-            coordinates = first['coordinates']
-            w.poly(coordinates)
-
-    for i in range(len(idxarr)):
-        w.record('polygon')
-
-    w.close()
-
-
-def savegeojson(shape):
-    gdf = geopandas.GeoSeries([Polygon([]) for i in range(len(shape.shapeRecords()))])
-    for s in range(len(shape.shapeRecords())):
-        feature = shape.shapeRecords()[s]
-        first = feature.shape.__geo_interface__
-        coordinates = first['coordinates'][0]
-        gdf[s] = Polygon(coordinates)
-
+def savefile(gpd, saveroute):
+    gdf = geopandas.GeoDataFrame([])
     gdf.crs = 32652
+    for i in range(len(gpd)):
+        if i in idxarr:
+            gdf = gdf.append(geopandas.GeoDataFrame([gpd.iloc[i, :]]))
+    gdf.to_file(saveroute + '.shp')
     gdf.to_file(saveroute + '.geojson', driver='GeoJSON')
 
 # 주천강 -> 죽동천597, 주천강610, 광려천 777
@@ -43,9 +18,5 @@ idxarr = [596, 609, 776]
 saveroute = 'new_유역도'
 
 # read 유역도
-shape = shapefile.Reader('유역도_지적도/유역도 52N.shp')
-saveshapefile(shape, saveroute)
-
-# shapefile -> geojson
-shape = shapefile.Reader(saveroute)
-savegeojson(shape)
+gpd = geopandas.GeoDataFrame.from_file('유역도_지적도/유역도 52N.shp', encoding='cp949')
+savefile(gpd, saveroute)
